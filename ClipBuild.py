3bfc5090bb0818336c4fd17ad976b075064409df
Lavan Vivekanandasarma <lavan.vivek11@gmail.com>
2023-04-24 10:43:16 -0600

    Generates subtitles

--- ClipBuild.py
+++ ClipBuild.py
@@ -2,128 +2,88 @@
 '''
 This file is used to build the final clip with the subtitles overlayed on top of the video clip.
 '''
-import sys
-import pysrt
-import io
+
+# clip_path = "/Users/lavan/Desktop/Python Things hissssss/Video-Creation-Tool/Video-Creation-Tool/ValorantClip.mp4"
+
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/lavan/Downloads/Dont Delete/APIKeys/GCPSpeechToText/video-creation-tool-de8442b66120.json"
+
 import os
-from google.cloud import speech_v1p1beta1 as speech
+import subprocess
+import io
+from typing import List, Tuple
 from moviepy.editor import *
 from moviepy.video.tools.subtitles import SubtitlesClip
-from moviepy.video.io.VideoFileClip import VideoFileClip
+from google.cloud import speech_v1p1beta1 as speech
+from google.cloud.speech_v1p1beta1 import SpeechClient, RecognitionConfig, RecognitionAudio
+from google.api_core.exceptions import InvalidArgument
 
-clip_path = "/Users/lavan/Desktop/Python Things hissssss/Video-Creation-Tool/Video-Creation-Tool/ValorantClip.mp4"
+def authenticate_google_cloud():
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/lavan/Downloads/Dont Delete/APIKeys/GCPSpeechToText/video-creation-tool-de8442b66120.json'
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/lavan/Downloads/Dont Delete/APIKeys/GCPSpeechToText/video-creation-tool-de8442b66120.json"
+def extract_audio(video_file, audio_file):
+    subprocess.run(['ffmpeg', '-i', video_file, '-vn', '-acodec', 'pcm_s16le', '-ar', '16000', '-ac', '1', audio_file])
 
-video = VideoFileClip(clip_path)
-audio = video.audio
-if audio is None:
-    print("Error: The video does not have an audio track.")
-    sys.exit(1)
-print("Audio track found - transcribing audio...")
-def transcribe_audio(video):
-    client = speech.SpeechClient()
+def transcribe_audio_to_subtitles(video_file):
+    authenticate_google_cloud()
+    audio_file = 'extracted_audio.wav'
+    extract_audio(video_file, audio_file)
 
-    # The language of the video's audio
-    language_code = "en-US"
+    client = SpeechClient()
 
-    # The encoding of the video's audio
-    audio_encoding = speech.RecognitionConfig.AudioEncoding.ENCODING_UNSPECIFIED
+    with io.open(audio_file, 'rb') as audio_file:
+        content = audio_file.read()
 
-    filename, ext = os.path.splitext(video.audio.filename)
-    if ext == ".aac":
-        audio_encoding = speech.RecognitionConfig.AudioEncoding.AAC
-    else:
-        audio_encoding = speech.RecognitionConfig.AudioEncoding.LINEAR16
-
-    config = speech.RecognitionConfig(
-        encoding=audio_encoding,
-        sample_rate_hertz=video.audio.fps,
-        language_code=language_code,
-        enable_word_time_offsets=True,
+    audio = RecognitionAudio(content=content)
+    config = RecognitionConfig(
+        encoding=RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=16000,
+        language_code='en-US',
+        enable_automatic_punctuation=True,
+        enable_word_time_offsets=True
     )
 
-    # Set up the audio stream for the video
-    audio = video.audio.to_soundarray()
+    try:
+        response = client.recognize(config=config, audio=audio)
+    except InvalidArgument:
+        operation = client.long_running_recognize(config=config, audio=audio)
+        response = operation.result()
 
-    # Call the Google Cloud Speech-to-Text API to transcribe the audio
-    def audio_chunks(audio, chunk_size=16000):
-        audio_bytes = audio.tobytes()
-        for i in range(0, len(audio_bytes), chunk_size):
-            yield audio_bytes[i:i + chunk_size]
+    subtitles = []
+    for result in response.results:
+        for word_info in result.alternatives[0].words:
+            word = word_info.word
+            start_time = word_info.start_time.total_seconds()
+            end_time = word_info.end_time.total_seconds()
+            subtitles.append((word, start_time, end_time))
 
-    # Update the requests variable to use the generator function
-    requests = (
-        speech.StreamingRecognizeRequest(audio_content=chunk)
-        for chunk in audio_chunks(audio)
-    )
-
-    streaming_config = speech.StreamingRecognitionConfig(
-        config=config,
-        interim_results=False,
-    )
-
-    responses = client.streaming_recognize(requests=requests, config=streaming_config)
-
-    # Parse the transcript and create a subtitle file
-    subs = pysrt.SubRipFile()
-    subs_idx = 1
-    current_phrase = ""
-    start_time = 0
-    end_time = 0
-    for response in responses:
-        for result in response.results:
-            if not result.is_final:
-                continue
-
-            for i, word in enumerate(result.alternatives[0].words):
-                if i == 0:
-                    start_time = int(word.start_time.seconds * 1000 + word.start_time.nanos / 1000000)
-                current_phrase += " " + word.word
-                end_time = int(word.end_time.seconds * 1000 + word.end_time.nanos / 1000000)
-
-            subs.append(pysrt.SubRipItem(index=subs_idx, start=pysrt.SubRipTime(milliseconds=start_time), end=pysrt.SubRipTime(milliseconds=end_time), text=current_phrase.strip()))
-            subs_idx += 1
-            current_phrase = ""
-
-    return subs
+    return subtitles
 
 
-subs = transcribe_audio(video)
+def write_subtitles_to_srt(subtitles: List[Tuple[str, float, float]], output_file: str):
+    with open(output_file, 'w', encoding='utf-8') as srt_file:
+        index = 1
+        for i, (word, start_time, end_time) in enumerate(subtitles):
+            if i == 0 or subtitles[i-1][2] != start_time:
+                srt_file.write(f"{index}\n")
+                index += 1
+                srt_file.write(f"{format_timestamp(start_time)} --> {format_timestamp(end_time)}\n")
+            srt_file.write(f"{word} ")
 
-print("Transcription complete - saving subtitles...")
-subs.save("subtitles.srt", encoding="utf-8")
-print("Subtitles saved - overlaying subtitles on video...")
-# Load your video file and extract the audio track
-video = VideoFileClip(clip_path)
-audio = video.audio
-
-# Parse the subtitle file using pysrt and extract the subtitle entries
-subtitle_file = pysrt.open("subtitles.srt")
-subtitle_entries = []
-for entry in subtitle_file:
-    start_time = timedelta(milliseconds=entry.start.ordinal)
-    end_time = timedelta(milliseconds=entry.end.ordinal)
-    text = entry.text
-    subtitle_entries.append(((start_time, end_time), text))
-
-# Check if the subtitle_entries list is empty
-if not subtitle_entries:
-    print("Error: No subtitle entries found.")
-    sys.exit(1)
-
-# Create a SubtitlesClip object using the subtitle entries and the audio track
-subtitles = SubtitlesClip(subtitle_entries, video.size)
-subtitles = subtitles.set_audio(audio)
-
-# Overlay the subtitles onto the video clip using the CompositeVideoClip method
-final_clip = CompositeVideoClip([video, subtitles])
-print("Subtitles overlayed - saving video...")
-
-# Write the final clip to a file
-final_clip.write_videofile("final_clip.mp4")
-print("Video saved - done.")
+            if i == len(subtitles) - 1 or subtitles[i+1][1] != end_time:
+                srt_file.write("\n\n")
 
 
+def format_timestamp(timestamp: float) -> str:
+    hours, remainder = divmod(timestamp, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return f"{int(hours):02d}:{int(minutes):02d}:{seconds:06.3f}".replace('.', ',')
+
+video_file = 'ValorantClip.mp4'
+subtitles = transcribe_audio_to_subtitles(video_file)
+
+print(subtitles)
+
+output_srt_file = 'subtitles.srt'
+write_subtitles_to_srt(subtitles, output_srt_file)
 
 
