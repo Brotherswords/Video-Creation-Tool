e8595e7cab057c73a4484ed3016ac0979d643dc6
Lavan Vivekanandasarma <lavan.vivek11@gmail.com>
2023-04-26 15:51:58 -0600

    Version with Google API endpoint and writing function working

--- ClipBuild.py
+++ ClipBuild.py
@@ -10,9 +10,11 @@ This file is used to build the final clip with the subtitles overlayed on top of
 import os
 import subprocess
 import io
+import pysrt
 from typing import List, Tuple
 from moviepy.editor import *
 from moviepy.video.tools.subtitles import SubtitlesClip
+from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
 from google.cloud import speech_v1p1beta1 as speech
 from google.cloud.speech_v1p1beta1 import SpeechClient, RecognitionConfig, RecognitionAudio
 from google.api_core.exceptions import InvalidArgument
@@ -58,7 +60,6 @@ def transcribe_audio_to_subtitles(video_file):
 
     return subtitles
 
-
 def write_subtitles_to_srt(subtitles: List[Tuple[str, float, float]], output_file: str):
     with open(output_file, 'w', encoding='utf-8') as srt_file:
         index = 1
@@ -72,11 +73,43 @@ def write_subtitles_to_srt(subtitles: List[Tuple[str, float, float]], output_fil
             if i == len(subtitles) - 1 or subtitles[i+1][1] != end_time:
                 srt_file.write("\n\n")
 
-
 def format_timestamp(timestamp: float) -> str:
     hours, remainder = divmod(timestamp, 3600)
     minutes, seconds = divmod(remainder, 60)
     return f"{int(hours):02d}:{int(minutes):02d}:{seconds:06.3f}".replace('.', ',')
+
+def srt_time_to_seconds(srt_time):
+    return srt_time.hours * 3600 + srt_time.minutes * 60 + srt_time.seconds + srt_time.milliseconds / 1000.0
+
+def add_subtitles_to_video(video_path, subtitles_path, output_path):
+    # Load subtitles
+    subs = pysrt.open(subtitles_path)
+    
+    # Load video
+    video = VideoFileClip(video_path)
+    
+    # Iterate through subtitles and create TextClips for each subtitle
+    subtitle_clips = []
+    for sub in subs:
+        start_time = srt_time_to_seconds(sub.start)
+        end_time = srt_time_to_seconds(sub.end)
+        duration = end_time - start_time
+
+        txt_clip = TextClip(sub.text,
+                            fontsize=24,
+                            color='white',
+                            bg_color='black',
+                            transparent_bg=True,
+                            size=video.size,
+                            print_cmd=True).set_position(('center', 'bottom')).set_duration(duration).set_start(start_time)
+
+        subtitle_clips.append(txt_clip)
+
+    # Overlay subtitles on video
+    final_clip = CompositeVideoClip([video] + subtitle_clips)
+    
+    # Write the output video file
+    final_clip.write_videofile(output_path, codec='libx264', audio_codec='aac')
 
 video_file = 'ValorantClip.mp4'
 subtitles = transcribe_audio_to_subtitles(video_file)
@@ -85,5 +118,9 @@ print(subtitles)
 
 output_srt_file = 'subtitles.srt'
 write_subtitles_to_srt(subtitles, output_srt_file)
+
+add_subtitles_to_video(video_file, output_srt_file, 'output_path.mp4')
+
+
 
 
