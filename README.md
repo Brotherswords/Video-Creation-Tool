c6741a23127d63cd1a17de25a913fdee62f8cbb2
Lavan Vivekanandasarma <lavan.vivek11@gmail.com>
2023-04-27 22:03:04 -0600

    Working ver with Speechmatic

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Video Creation Tool
+
+# README
+
+This script is used to generate subtitles and overlay them onto a video clip using the Speechmatics API and the ffmpeg command-line tool.
+
+# Prerequisites
+
+Before running this script, you need to have the following installed on your system:
+
+# Python 3.6 or higher
+
+The speechmatics and httpx Python packages (you can install them using pip)
+The ffmpeg command-line tool
+You also need to have a Speechmatics API key, which you can obtain from the Speechmatics website.
+
+# Usage
+
+Set the API_KEY, PATH_TO_FILE, and LANGUAGE variables at the top of the script to the appropriate values for your use case.
+
+Run the script using the following command:
+
+'''
+python generate_subtitles.py
+'''
+
+The script will submit a transcription job to the Speechmatics API and wait for the results. Once the results are available, it will generate an SRT file containing the subtitles and overlay them onto the video clip using ffmpeg. The output video file will be named output.mp4.
--- SubtitleGeneration.py
+++ SubtitleGeneration.py
@@ -3,126 +3,86 @@
 This file is used to build the final clip with the subtitles overlayed on top of the video clip.
 '''
 
-import os
+import json
 import subprocess
-import io
-import pysrt
-from typing import List, Tuple
-from moviepy.editor import *
-from moviepy.video.tools.subtitles import SubtitlesClip
-from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
-from google.cloud import speech_v1p1beta1 as speech
-from google.cloud.speech_v1p1beta1 import SpeechClient, RecognitionConfig, RecognitionAudio
-from google.api_core.exceptions import InvalidArgument
+import speechmatics as Speechmatics
+import APIKey
+from speechmatics.models import ConnectionSettings
+from speechmatics.batch_client import BatchClient
+from httpx import HTTPStatusError
+
 
 #Video file path for which subtitles are to be generated
-video_file = 'ValorantClip.mp4'
+PATH_TO_FILE = "ValClips.mp4"
+LANGUAGE = "en"
+json_data = None
 
-# Function to authenticate the Google Cloud API 
-# Note: file should be service key json generated for Speech to Text API
-def authenticate_google_cloud():
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/lavan/Downloads/Dont Delete/APIKeys/GCPSpeechToText/video-creation-tool-de8442b66120.json'
+# Use the Speechmatics API key in Speechmatics.APIKey
+settings = ConnectionSettings(
+    url="https://asr.api.speechmatics.com/v2",
+    auth_token=APIKey.API_KEY,
+)
 
-# Function to extract audio from the video file in desired format
-def extract_audio(video_file, audio_file):
-    subprocess.run(['ffmpeg', '-i', video_file, '-vn', '-acodec', 'pcm_s16le', '-ar', '16000', '-ac', '1', audio_file])
+# Define transcription parameters
+conf = {
+    "type": "transcription",
+    "transcription_config": {
+        "language": LANGUAGE
+    }
+}
 
-# Function to transcribe the audio file to subtitles from Google Cloud Speech to Text API
-# -> Returns a list of tuples of the form (word, start_time, end_time)
-def transcribe_audio_to_subtitles(video_file):
-    authenticate_google_cloud()
-    audio_file = 'extracted_audio.wav'
-    extract_audio(video_file, audio_file)
-
-    client = SpeechClient()
-
-    with io.open(audio_file, 'rb') as audio_file:
-        content = audio_file.read()
-
-    audio = RecognitionAudio(content=content)
-    config = RecognitionConfig(
-        encoding=RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=16000,
-        language_code='en-US',
-        enable_automatic_punctuation=True,
-        enable_word_time_offsets=True
-    )
-
+# Open the client using a context manager
+with BatchClient(settings) as client:
     try:
-        response = client.recognize(config=config, audio=audio)
-    except InvalidArgument:
-        operation = client.long_running_recognize(config=config, audio=audio)
-        response = operation.result()
+        job_id = client.submit_job(
+            audio=PATH_TO_FILE,
+            transcription_config=conf,
+        )
+        print(f'job {job_id} submitted successfully, waiting for transcript')
 
-    subtitles = []
-    for result in response.results:
-        for word_info in result.alternatives[0].words:
-            word = word_info.word
-            start_time = word_info.start_time.total_seconds()
-            end_time = word_info.end_time.total_seconds()
-            subtitles.append((word, start_time, end_time))
+        # Note that in production, you should set up notifications instead of polling.
+        # Notifications are described here: https://docs.speechmatics.com/features-other/notifications
+        transcript = client.wait_for_completion(job_id, transcription_format='json-v2')
+        # To see the full output, try setting transcription_format='json-v2'.
+        print(transcript)
 
-    return subtitles
+        # Parse the JSON response
+        timestamps = []
+        transcriptions = []
+        for item in transcript['results']:
+            if item['type'] == 'word':
+                start_time = item['start_time']
+                end_time = item['end_time']
+                transcriptions.append(item['alternatives'][0]['content'])
+                timestamps.append((start_time, end_time))
 
-#Creates SRT file with subtitles overlayed on top of the video clip
-def write_subtitles_to_srt(subtitles: List[Tuple[str, float, float]], output_file: str):
-    with open(output_file, 'w', encoding='utf-8') as srt_file:
-        index = 1
-        for i, (word, start_time, end_time) in enumerate(subtitles):
-            if i == 0 or subtitles[i-1][2] != start_time:
-                srt_file.write(f"{index}\n")
-                index += 1
-                srt_file.write(f"{format_timestamp(start_time)} --> {format_timestamp(end_time)}\n")
-            srt_file.write(f"{word} ")
+        # Generate the SRT file
+        srt = ''
+        for i in range(len(timestamps)):
+            start = '{:02d}:{:02d}:{:02d},{:03d}'.format(
+                int(timestamps[i][0])//3600, int(timestamps[i][0])//60%60,
+                int(timestamps[i][0])%60, int((timestamps[i][0] - int(timestamps[i][0]))*1000))
+            end = '{:02d}:{:02d}:{:02d},{:03d}'.format(
+                int(timestamps[i][1])//3600, int(timestamps[i][1])//60%60,
+                int(timestamps[i][1])%60, int((timestamps[i][1] - int(timestamps[i][1]))*1000))
+            srt += f'{i+1}\n{start} --> {end}\n{transcriptions[i]}\n\n'
 
-            if i == len(subtitles) - 1 or subtitles[i+1][1] != end_time:
-                srt_file.write("\n\n")
+        with open('output.srt', 'w') as f:
+            f.write(srt)
 
-def format_timestamp(timestamp: float) -> str:
-    hours, remainder = divmod(timestamp, 3600)
-    minutes, seconds = divmod(remainder, 60)
-    return f"{int(hours):02d}:{int(minutes):02d}:{seconds:06.3f}".replace('.', ',')
+        # Overlay the SRT file onto the video
+        subprocess.run(['ffmpeg', '-i', 'ValClipShort.mp4', '-vf', f'subtitles=output.srt', '-c:a', 'copy', 'output.mp4'])
 
-def srt_time_to_seconds(srt_time):
-    return srt_time.hours * 3600 + srt_time.minutes * 60 + srt_time.seconds + srt_time.milliseconds / 1000.0
+    except HTTPStatusError as e:
+        if e.response.status_code == 401:
+            print('Invalid API key - Check your API_KEY at the top of the code!')
+        elif e.response.status_code == 400:
+            print(e.response.json()['detail'])
+            json_data = e.response.json()
+        else:
+            raise e
 
-# Reads SRT file to perform subtitles overlaying on top of the video clip
-def add_subtitles_to_video(video_path, subtitles_path, output_path):
-    # Load subtitles
-    subs = pysrt.open(subtitles_path)
-    
-    # Load video
-    video = VideoFileClip(video_path)
-    
-    # Iterate through subtitles and create TextClips for each subtitle
-    subtitle_clips = []
-    for sub in subs:
-        start_time = srt_time_to_seconds(sub.start)
-        end_time = srt_time_to_seconds(sub.end)
-        duration = end_time - start_time
 
-        txt_clip = TextClip(sub.text,
-                            fontsize=24,
-                            color='white',
-                            bg_color='transparent',
-                            size=video.size, align='South',
-                            print_cmd=True).set_position(('center', 'bottom')).set_duration(duration).set_start(start_time)
 
-        subtitle_clips.append(txt_clip)
-
-    # Overlay subtitles on video
-    final_clip = CompositeVideoClip([video] + subtitle_clips)
-    
-    # Write the output video file
-    final_clip.write_videofile(output_path, codec='libx264', audio_codec='aac')
-
-subtitles = transcribe_audio_to_subtitles(video_file)
-
-print(subtitles)
-
-output_srt_file = 'subtitles.srt'
-
-write_subtitles_to_srt(subtitles, output_srt_file)
-
-add_subtitles_to_video(video_file, output_srt_file, 'output_path.mp4')
+# Parse the JSON response
 
